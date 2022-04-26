5c5798dbcf719bc67c4ccc1fbee104f7f93bfd8b
Rainer Brieger <hello@rainerb.com>
2022-04-26 13:56:16 -0600

    Adding mail social icon, changing color of icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,18 +8,31 @@ const Footer = () => {
       <div className="social-links">
         <SocialIcon
           url="https://www.facebook.com/klvsaudio"
+          network="facebook"
+          bgColor="#2B2B2B"
           className="social-item"
           target="_blank"
           style={{ height: 35, width: 35 }}
         />
         <SocialIcon
           url="https://www.instagram.com/klvs.audio"
+          network="instagram"
+          bgColor="#2B2B2B"
           className="social-item"
           target="_blank"
           style={{ height: 35, width: 35 }}
         />
         <SocialIcon
           url="https://soundcloud.com/klvsmastering"
+          network="soundcloud"
+          bgColor="#2B2B2B"
+          className="social-item"
+          target="_blank"
+          style={{ height: 35, width: 35 }}
+        />
+        <SocialIcon
+          url="mailto:info@klvsaudio.co"
+          bgColor="#2B2B2B"
           className="social-item"
           target="_blank"
           style={{ height: 35, width: 35 }}
